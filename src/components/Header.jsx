5e8cbfc890eb74339d0eab90e0bc9ff6260662d2
Sergey-Kozlov-developer <vmf.serge.kozlov@gmail.com>
2023-10-30 16:13:53 +0300

    add home section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,26 +4,37 @@ import { Flex, Heading, Image, Link } from "@chakra-ui/react";
 import logo from "./../assets/img/logo/logo.svg";
 
 const Header = () => {
+	const navLinks = [
+		{ page: "Home" },
+		{ page: "About" },
+		{ page: "Portfolio" },
+		{ page: "Contact" },
+	];
 	return (
 		<>
-			<Flex justifyContent="space-between" pt={4} alignItems={"center"}>
+			<Flex
+				justifyContent="space-between"
+				pt={4}
+				alignItems={"center"}
+				mb={"48"}
+			>
 				<Flex gap="4">
 					<Image src={logo} width={10} />
 					<Heading as={"h1"}>DVLPR</Heading>
 				</Flex>
 				<Flex gap={"36"}>
-					<Link href="#!" _hover={{ textDecoration: "none" }}>
-						Home
-					</Link>
-					<Link href="#!" _hover={{ textDecoration: "none" }}>
-						About
-					</Link>
-					<Link href="#!" _hover={{ textDecoration: "none" }}>
-						Portfolio
-					</Link>
-					<Link href="#!" _hover={{ textDecoration: "none" }}>
-						Contact
-					</Link>
+					{navLinks.map((navLink, index) => (
+						<Link
+							key={index}
+							href="#!"
+							_hover={{
+								textDecoration: "none",
+								color: "purple.400",
+							}}
+						>
+							{navLink.page}
+						</Link>
+					))}
 				</Flex>
 			</Flex>
 		</>
