d3d7f7f8a7de81c316bc0ecdc3962a21d997966f
Sergey-Kozlov-developer <vmf.serge.kozlov@gmail.com>
2023-11-02 15:18:55 +0300

    up skills section

--- src/components/skills/index.jsx
+++ src/components/skills/index.jsx
@@ -4,6 +4,9 @@ import reduxImage from "./../../assets/img/skills/redux.svg";
 import flutterImage from "./../../assets/img/skills/flutter.svg";
 import dartImage from "./../../assets/img/skills/dart.svg";
 import bobxImage from "./../../assets/img/skills/mobx.svg";
+import sassImage from "./../../assets/img/skills/sass.svg";
+import tailwImage from "./../../assets/img/skills/tailwind.svg";
+import gitImage from "./../../assets/img/skills/github.svg";
 
 const Skills = () => {
 	const items = [
@@ -13,14 +16,17 @@ const Skills = () => {
 		{ id: 4, image: `${flutterImage}`, title: "Flutter" },
 		{ id: 5, image: `${dartImage}`, title: "Dart" },
 		{ id: 6, image: `${bobxImage}`, title: "BobX" },
+		{ id: 7, image: `${sassImage}`, title: "Sass" },
+		{ id: 8, image: `${tailwImage}`, title: "Tailwind" },
+		{ id: 9, image: `${gitImage}`, title: "GitHub" },
 	];
 
 	return (
-		<section className="mx-auto mb-56 mt-60 grid w-full grid-cols-3 items-center justify-items-center gap-20">
+		<section className="mx-auto mb-56 mt-56 grid w-full grid-cols-3 items-center justify-items-center gap-20">
 			{items.map((item) => (
 				<div
 					key={item.id}
-					className="flex h-64 w-64 flex-col items-center justify-center rounded-2xl bg-stone-800"
+					className="flex h-52 w-52 flex-col items-center justify-center rounded-2xl bg-stone-800"
 				>
 					<img
 						src={item.image}
